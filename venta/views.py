c411cdc5fdf0cb47e82f31d566ffdb4980c66579
frappuccinoic <arseeicore@gmail.com>
2025-10-13 10:47:14 -0300

    Implementación de paginas blog y category, usando modelos Post de producto y Categorias

--- venta/views.py
+++ venta/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import PostProduct, Category
 
-# Create your views here.
+def blog(req):
+    posts = PostProduct.objects.all()
+    return render(req, 'venta/blog.html', {"posts": posts})
+
+def category(req, category_id):
+    ctg = get_object_or_404(Category,id = category_id)
+    return render(req, 'venta/category.html', {"ctg": ctg})
