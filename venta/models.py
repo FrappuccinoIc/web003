a9bad2cb8e078bf2d1363a270267a026a5361561
frappuccinoic <arseeicore@gmail.com>
2025-10-13 08:33:00 -0300

    Modelo categoria creada

--- venta/models.py
+++ venta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 50, verbose_name = "Nombre")
+    
+    created = models.DateTimeField(auto_now = True, verbose_name = "Fecha de creación")
+    updated = models.DateTimeField(auto_now_add = True, verbose_name = "Fecha de edición")
+    
+    class Meta:
+        verbose_name = "categoria"
+        verbose_name_plural = "categorias"
+
+    def __str__(self): return self.name
