7ea487fcad4dcf9fe56efcc55f9ee3d342c36711
Frappuccinoic <arseeicore@gmail.com>
2025-10-25 21:39:12 -0300

    Añadido imágenes a posts de blog

--- web003/urls.py
+++ web003/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from venta import views as views_venta
 from core import views as views_core
 
@@ -9,3 +10,7 @@ urlpatterns = [
     path('category/<int:category_id>', views_venta.category, name = "category"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
