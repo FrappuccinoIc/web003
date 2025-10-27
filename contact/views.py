061b0bd670412033f2b9f83cf81a333bbb9ec542
frappuccinoic <arseeicore@gmail.com>
2025-10-27 10:00:04 -0300

    Validación de formulario y redireccionamiento al validar formulario

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import ContactForm
 
 def contact(req):
     contact_form = ContactForm
+    if req.method == "POST":
+        contact_form = contact_form(data = req.POST)
+        if contact_form.is_valid():
+            name = req.POST.get('name', '')
+            email = req.POST.get('email', '')
+            content = req.POST.get('content', '')
+            return redirect(reverse('contact') + '?ok')
+        
     return render(req, 'contact/contacto.html', {"form": contact_form})
